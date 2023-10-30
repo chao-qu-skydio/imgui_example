3d940d793d637101e2369a7d7b2ed3bc8f294cf3
chao-qu-skydio <chao.qu@skydio.com>
2023-10-30 11:13:41 -0700

    update

--- hello.cpp
+++
@@ -1,7 +0,0 @@
-#include <absl/log/log.h>
-#include <fmt/core.h>
-
-int main(int argc, char **argv) {
-  LOG(INFO) << fmt::format("Hello, world!");
-  LOG(WARNING) << fmt::format("Hello, world!");
-}
